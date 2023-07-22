7867197e9f4dea133c9c09fbc0fd1923fbe578e3
Charan-Cherry568 <140180568+Charan-Cherry568@users.noreply.github.com>
2023-07-22 11:55:14 +0530

    Update index.js

--- jscripts/index.js
+++ jscripts/index.js
@@ -4,34 +4,35 @@ document.getElementById('more_cat').addEventListener('click', () => {
 
 
 function sendEmail() {
-  if (String(document.getElementsByName("FullName").value).trim().length !=0 &&
-              String(document.getElementsByName("EventType").value).trim() !=0 &&
-              String(document.getElementsByName("District").value).trim() !=0 &&
-              String(document.getElementsByName("Pincode").value).trim() !=0 &&
-              String(document.getElementsByName("ContactNumber").value).trim() !=0 ) {
+ 
+  if (String(document.getElementById("FullName").value).trim().length !=0 &&
+              String(document.getElementById("EventType").value).trim() !=0 &&
+              String(document.getElementById("District").value).trim() !=0 &&
+              String(document.getElementById("Pincode").value).trim() !=0 &&
+              String(document.getElementById("ContactNumber").value).trim() !=0 ) {
 
-    var optional = String(document.getElementsByName("Options").value).trim();
+    var optional = String(document.getElementById("Options").value).trim();
     
     if (optional.length != 0) {
       optional = optional+" These are my extra optimizations."
      }
-    var text = "Hello Sir,\n I am "+document.getElementsByName("FullName").value +
-              "  from "+document.getElementsByName("District")+" District ,"+"Pincode "+document.getElementsByName("Pincode")+". I am Looking for "+document.getElementsByName("EventType").value+
+    var text = "Hello Sir,\n I am "+document.getElementById("FullName").value +
+              "  from "+document.getElementById("District")+" District ,"+"Pincode "+document.getElementById("Pincode")+". I am Looking for "+document.getElementById("EventType").value+
               " Event Manager. I am interested in your team. \n"+
-              "I would like to make a DEAL."+optional+" you can contact me via Mobile No: "+document.getElementsByName("ContactNumber").value+
+              "I would like to make a DEAL."+optional+" you can contact me via Mobile No: "+document.getElementById("ContactNumber").value+
               " or Mail. Looking forward for a quick reply. \nThank You.";
 
     mailWith(text);
     
-    document.getElementsByName("FullName").value = "";
-    document.getElementsByName("EventType").value = "";
-	document.getElementsByName("District").value = "";
-  	document.getElementsByName("Pincode").value = "";
-  	document.getElementsByName("Options").value = "";
-  	document.getElementsByName("ContactNumber").value = "";
+    document.getElementById("FullName").value = "";
+    document.getElementById("EventType").value = "";
+	  document.getElementById("District").value = "";
+  	document.getElementById("Pincode").value = "";
+  	document.getElementById("Options").value = "";
+  	document.getElementById("ContactNumber").value = "";
 
   }else{
-    alert("Please fill the * mark fields");
+    alert("Please fill the (*) mark fields");
   }
 
 }
